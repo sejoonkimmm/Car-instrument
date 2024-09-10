de04f6f6d93c25398b6c73547c51bbe85f55c0d4
Adinkra-Ideas <musicjeejo@gmail.com>
2024-09-10 10:30:19 +0200

    Troubleshooting Needle Movement Error

--- src/CANBusReader.cpp
+++ src/CANBusReader.cpp
@@ -16,8 +16,12 @@ CANBusReader::CANBusReader(QString interface, QObject *parent)
             qDebug() << errorString0;
         else {
             qDebug() << "Available interfaces";
-            foreach(auto &x, devices)
-                qDebug()<<x.name();
+            foreach(auto &x, devices) {
+                qDebug()<< "name: " << x.name();
+                qDebug()<< "alias: " << x.alias();
+                qDebug()<< "channel(): " << x.channel();
+                qDebug()<< "serialNumber() " << x.serialNumber();
+            }
         }
 
 
